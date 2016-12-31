0cf23e9e03f34e92fd862ffe60e4ad5c3b2746e6
Joseph Jung <zechanimal@gmail.com>
2016-12-30 23:07:30 -0600

    Add Message model and initial channel scaffolding

---
+++ BestGirl/BestGirlBot/Discord/Converters/NullableSnowflakeJsonConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Globalization;
+
+namespace BestGirlBot.Discord.Converters
+{
+	class NullableSnowflakeJsonConverter : JsonConverter
+	{
+		public override bool CanConvert(Type objectType)
+		{
+			return objectType == typeof(ulong?);
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			return reader.Value == null ? (ulong?)null : ulong.Parse(((string)reader.Value), NumberStyles.None, CultureInfo.InvariantCulture);
+		}
+
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			writer.WriteValue(value == null ? null : ((ulong)value).ToString(CultureInfo.InvariantCulture));
+		}
+	}
+}
---
+++ BestGirl/BestGirlBot/Discord/Rest/Repositories/IChannelRepository.cs
@@ -0,0 +1,19 @@
+﻿using BestGirlBot.Discord.Models;
+
+namespace BestGirlBot.Discord.Rest.Repositories
+{
+	public interface IChannelRepository
+	{
+		Channel GetChannel(ulong channelId);
+		void ModifyChannel(ulong channelId, string name, int position);
+		void ModifyChannel(ulong channelId, string name, int position, string topic);
+		void ModifyChannel(ulong channelId, string name, int position, int bitrate, int userLimit);
+		void DeleteChannel(ulong channelId);
+		Message[] GetChannelMessages(ulong channelId);
+		Message GetChannelMessage(ulong channelId, ulong messageId);
+		void CreateChannelMessage(ulong channelId, string content);
+		void EditMessage(ulong channelId, ulong messageId, string content);
+		void DeleteMessage(ulong channelId, ulong messageId);
+		void BulkDeleteMessages(ulong channelId, ulong[] messageIds);
+	}
+}
